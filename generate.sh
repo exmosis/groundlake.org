67486168e575b5d2a410b32fd36c79d7b9679d74
Graham <janus-git@exmosis.net>
2021-11-23 09:47:57 +0000

    Separate out build scripts

---
+++ generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# markdown-styles: https://github.com/mixu/markdown-styles#readme
+
+# generate-md --layout jasonm23-markdown --input ./md --output ./html
+# generate-md --layout mixu-gray --input ./md --output ./html
+
+# generate-md --layout jasonm23-foghorn --input ./md --output ./html
+
+generate-md --layout ./groundlake_theme --input ./md --output ./html
